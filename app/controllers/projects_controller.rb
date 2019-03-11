545ddf48bb6da9eb98fdfc65e4be187ab00feb86
Jonathan <jon1969edwards@gmail.com>
2019-03-11 13:39:16 +0100

    Creates controllers and models for adding projects to web app

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,24 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all.order("created_at desc")
+  end
+  def new
+    @project = Project.new
+  end
+  def create
+    @project = Project.new project_params
+
+    if @project.save
+      redirect_to @project, notice: "Saved!"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :link)
+  end
+end
+
