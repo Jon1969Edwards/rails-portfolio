c6bc559a1b7396cf6d0ddf17f501f1e7933b26ea
Jonathan <jon1969edwards@gmail.com>
2019-03-21 13:49:37 +0100

    Generates 'about_me' controller and index file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   resources :projects
+  resources :about_me
+    get 'about_me' => 'about_me#index'
   get '/', to: 'homepage#home', as: 'root'
 end
